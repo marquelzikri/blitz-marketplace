ec100799a7ad112158d4985baca98c1046f375c7
Muhammad Zikri Cirrus Marquel Bismar <33832889+marquelzikri@users.noreply.github.com>
2021-06-27 01:49:26 +0700

    Created profile layout component (#8)

--- app/components/ZodForm.tsx
+++ app/components/ZodForm.tsx
@@ -26,6 +26,7 @@ export function ZodForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
                 name={fieldName}
                 label={fieldLabel}
                 placeholder={fieldLabel}
+                invisible={fieldName === "id"}
               />
             )
           })
