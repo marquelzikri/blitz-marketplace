8c4974cc56be46f6fa4ef66bd539c61854645701
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2021-06-29 03:16:45 +0700

    Revert LabeledTextField

--- app/components/ZodForm.tsx
+++ app/components/ZodForm.tsx
@@ -1,36 +1,66 @@
+import { CSSProperties } from "react"
+import { z, ZodArray, ZodBigInt, ZodDate, ZodNumber, ZodObject, ZodOptional, ZodString } from "zod"
+
 import { Form, FormProps } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
-import { z } from "zod"
+
 export { FORM_ERROR } from "app/core/components/Form"
 
 export function ZodForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   const { schema } = props
-  return (
-    <Form<S> {...props}>
-      {schema
-        ? Object.keys(schema["shape"]).map((fieldName) => {
-            const field = schema["shape"][fieldName]
-            if (field == null) return null
 
-            let fieldLabel = fieldName.replace(/([A-Z])/g, " $1")
-            fieldLabel = fieldLabel.charAt(0).toUpperCase() + fieldLabel.slice(1)
+  const iterateSchema = (schema: any) =>
+    Object.keys(schema["shape"]).map((fieldName) => renderField(schema, fieldName))
 
-            const fieldType = field?.__proto__?.constructor?.name
-            let type: "number" | "text" | "email" | "password" | undefined = "text"
-            if (fieldType === "ZodNumber") type = "number"
+  const getFieldType = (obj: any) => {
+    console.log(obj)
+    switch (obj?.constructor) {
+      case ZodDate:
+      case ZodString:
+        return "text"
+      case ZodBigInt:
+      case ZodNumber:
+        return "number"
+      // case ZodDate:
+      //   return "date"
+      case ZodArray:
+        return "array"
+      case ZodObject:
+      case ZodOptional:
+        return getFieldType(obj?._def?.innerType)
+      default:
+        break
+    }
+  }
 
-            return (
-              <LabeledTextField
-                key={fieldName}
-                type={type}
-                name={fieldName}
-                label={fieldLabel}
-                placeholder={fieldLabel}
-                invisible={fieldName === "id"}
-              />
-            )
-          })
-        : null}
-    </Form>
-  )
+  const renderField = (schema: any, fieldName: string) => {
+    const field = schema["shape"][fieldName]
+    if (field == null) return null
+
+    let fieldLabel = fieldName.replace(/([A-Z])/g, " $1")
+    fieldLabel = fieldLabel.charAt(0).toUpperCase() + fieldLabel.slice(1)
+
+    const type = getFieldType(field)
+    if (type === "array") {
+      return iterateSchema(field?._def?.type)
+    }
+
+    const visibilityStyle: CSSProperties = {
+      visibility: "hidden",
+      position: "absolute",
+    }
+
+    return (
+      <LabeledTextField
+        key={fieldName}
+        type={type}
+        name={fieldName}
+        label={fieldLabel}
+        placeholder={fieldLabel}
+        outerProps={{ style: { ...(fieldName === "id" ? visibilityStyle : {}) } }}
+      />
+    )
+  }
+
+  return <Form<S> {...props}>{schema ? iterateSchema(schema) : null}</Form>
 }
