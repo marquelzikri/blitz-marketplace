9e2a1f0fad780faab89486000921d3d4d68e30b4
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2021-07-02 06:42:11 +0700

    Created seed file

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,5 @@
-// import db from "./index"
+import { SecurePassword } from "blitz"
+import db from "./index"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -8,9 +9,101 @@
  * realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  // Delete all existing data
+  await db.category.deleteMany()
+  await db.variant.deleteMany()
+  await db.rating.deleteMany()
+  await db.product.deleteMany()
+  await db.membership.deleteMany()
+  await db.organization.deleteMany()
+  await db.address.deleteMany()
+  await db.token.deleteMany()
+  await db.session.deleteMany()
+  await db.user.deleteMany()
+
+  // Create default store/shop
+  const storeName = process.env.STORE_NAME || "blitz_commerce"
+  const defaultStore = await db.organization.create({
+    data: {
+      name: storeName,
+      permalink: storeName.toLowerCase().split(" ").join("_"),
+    },
+  })
+
+  // Create user
+  const hashedPassword = await SecurePassword.hash("123456789ab".trim())
+  const user = await db.user.create({
+    data: {
+      email: "user1@mail.com",
+      hashedPassword,
+      memberships: {
+        create: {
+          role: "USER",
+          isDefault: false,
+          organization: {
+            connect: {
+              id: defaultStore.id,
+            },
+          },
+        },
+      },
+    },
+    select: { id: true, name: true, email: true, memberships: true },
+  })
+
+  // Create user's store/shop
+  const userStore = await db.organization.create({
+    data: {
+      name: "Store 1",
+      permalink: "store_1",
+    },
+  })
+  await db.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      memberships: {
+        create: {
+          role: "OWNER",
+          isDefault: true,
+          organization: {
+            connect: {
+              id: userStore.id,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  // Create user's first product
+  await db.product.create({
+    data: {
+      sku: "123",
+      title: "T-shirt",
+      permalink: "t-shirt",
+      organization: { connect: { id: userStore.id } },
+      variants: {
+        create: {
+          sku: "1231",
+          price: 10,
+          color: "white",
+          // dimensions in cm
+          height: 74.5,
+          width: 55.9,
+          length: 1,
+          weight: 0.5, // kg
+          stock: 10,
+        },
+      },
+      categories: {
+        create: {
+          name: "t-shirt",
+        },
+      },
+    },
+  })
 }
 
 export default seed
