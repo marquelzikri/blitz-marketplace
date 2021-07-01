c37e31ff6b610f20098c200d08b67950ccf07379
marquelzikri <zikrimarquel@student.telkomuniversity.ac.id>
2021-07-02 06:40:07 +0700

    Added select filter to getProducts query

--- app/products/queries/getProducts.ts
+++ app/products/queries/getProducts.ts
@@ -2,25 +2,27 @@ import { paginate, resolver } from "blitz"
 import db, { Prisma } from "db"
 
 interface GetProductsInput
-  extends Pick<Prisma.ProductFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+  extends Pick<Prisma.ProductFindManyArgs, "where" | "select" | "orderBy" | "skip" | "take"> {}
 
-export default resolver.pipe(async ({ where, orderBy, skip = 0, take = 100 }: GetProductsInput) => {
-  const {
-    items: products,
-    hasMore,
-    nextPage,
-    count,
-  } = await paginate({
-    skip,
-    take,
-    count: () => db.product.count({ where }),
-    query: (paginateArgs) => db.product.findMany({ ...paginateArgs, where, orderBy }),
-  })
+export default resolver.pipe(
+  async ({ where, select, orderBy, skip = 0, take = 100 }: GetProductsInput) => {
+    const {
+      items: products,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.product.count({ where }),
+      query: (paginateArgs) => db.product.findMany({ ...paginateArgs, where, orderBy, select }),
+    })
 
-  return {
-    products,
-    nextPage,
-    hasMore,
-    count,
+    return {
+      products,
+      nextPage,
+      hasMore,
+      count,
+    }
   }
-})
+)
